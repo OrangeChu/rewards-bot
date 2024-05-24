af013932d5458b46e1a77349b791d54dbf11dc28
OrangeChu <757581542@qq.com>
2024-05-25 07:01:51 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,11 @@ export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin
 # Check if RUN_ON_START is set to true and execute the script immediately
 if [ "$RUN_ON_START" = "true" ]; then
   echo "RUN_ON_START is set to true, executing run_daily.sh immediately..."
-  /usr/src/app/src/run_daily.sh
+  /usr/src/microsoft-rewards-script/src/run_daily.sh
 fi
 
 # Create a crontab file with the specified schedule
-echo "${CRON_SCHEDULE} /usr/src/app/src/run_daily.sh >> /var/log/cron.log 2>&1" > /etc/cron.d/crontab
+echo "${CRON_SCHEDULE} /usr/src/microsoft-rewards-script/src/run_daily.sh >> /var/log/cron.log 2>&1" > /etc/cron.d/crontab
 
 # Apply cron job
 crontab /etc/cron.d/crontab
